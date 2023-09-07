89882c522364365c2f8bb501b3b2b024e02bdc8f
R <108376638+RGcoderA@users.noreply.github.com>
2023-09-07 10:41:54 -0400

    commit 2

--- #Weather-App.py
+++ #Weather-App.py
@@ -15,4 +15,14 @@ root.resizable(False,False)
 
 #Search Box
 
+search_frame=Frame(root,bg="white")
+search_frame.place(x=0,y=0,width=900,height=50)
+
+search_entry=Entry(search_frame,font=("times new roman",15),bg="lightgray")
+search_entry.grid(row=0,column=0,padx=10,pady=10)
+
+search_button=Button(search_frame,text="Search",font=("times new roman",15),bg="lightblue",fg="white",cursor="hand2")
+search_button.grid(row=0,column=1,padx=10,pady=10)
+
 root.mainloop()
+
